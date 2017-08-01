128063e0f37a67abc6013be023f9e0a70b692138
bksharma13 <bsharma013@yahoo.com>
2017-08-01 21:16:05 +0530

    Added solution and required files

---
+++ PTEEassyPracticeApp/PTEEassyPracticeApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace PTEEassyPracticeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBoxEassy = new System.Windows.Forms.TextBox();
+            this.TestTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnClear);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStop);
+            this.splitContainer1.Panel1.Controls.Add(this.lblLength);
+            this.splitContainer1.Panel1.Controls.Add(this.lblTimer);
+            this.splitContainer1.Panel1.Controls.Add(this.btnStart);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtBoxEassy);
+            this.splitContainer1.Size = new System.Drawing.Size(484, 462);
+            this.splitContainer1.SplitterDistance = 58;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClear.Location = new System.Drawing.Point(404, 0);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 58);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Time Left :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total Words :";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(145, 32);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(86, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(341, 32);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(17, 17);
+            this.lblLength.TabIndex = 2;
+            this.lblLength.Text = "0";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(340, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(49, 17);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "00:00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtBoxEassy
+            // 
+            this.txtBoxEassy.AcceptsReturn = true;
+            this.txtBoxEassy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBoxEassy.Location = new System.Drawing.Point(0, 0);
+            this.txtBoxEassy.Multiline = true;
+            this.txtBoxEassy.Name = "txtBoxEassy";
+            this.txtBoxEassy.Size = new System.Drawing.Size(484, 400);
+            this.txtBoxEassy.TabIndex = 0;
+            this.txtBoxEassy.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TestTimer
+            // 
+            this.TestTimer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "PTE Eassy Writing App";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtBoxEassy;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer TestTimer;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ PTEEassyPracticeApp/PTEEassyPracticeApp/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PTEEassyPracticeApp
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch _timeKeeperStopwatch;
+        public Form1()
+        {
+            InitializeComponent();
+            TestTimer.Tick += new EventHandler(TestTimer_Tick);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            lblTimer.ForeColor = Color.Black;
+            _timeKeeperStopwatch = Stopwatch.StartNew();
+            lblTimer.Text = _timeKeeperStopwatch.Elapsed.ToString(@"mm\:ss");
+            lblLength.Text = "0";
+            txtBoxEassy.Text = string.Empty;
+            TestTimer.Enabled = true;
+            TestTimer.Interval = 1000;
+            TestTimer.Start();
+        }
+
+        void TestTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timetaken = _timeKeeperStopwatch.Elapsed;
+            lblTimer.Text = timetaken.ToString(@"mm\:ss");
+            if (timetaken.TotalMinutes >= 20)
+            {
+                lblTimer.ForeColor = Color.Red;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtBoxEassy.Text))
+            {
+                lblLength.Text = txtBoxEassy.Text.Split(' ').Length + "";
+            }
+            else
+            {
+                lblLength.Text = "0";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (TestTimer.Enabled)
+            {
+                _timeKeeperStopwatch.Stop();
+                TestTimer.Stop();
+                TestTimer.Enabled = false;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBoxEassy.Text = string.Empty;
+        }
+    }
+}
